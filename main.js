f63d2aee922f5842582f147f116312f889aaac3c
unnamedunknwn <unnamedsounknown@gmail.com>
2021-05-10 15:20:38 +0500

    add

---
+++ main.js
@@ -0,0 +1,93 @@
+var a = 1;
+
+function show_hide() {
+
+    if(a==1)
+        {
+            document.getElementById("showtext").style.display="inline";
+            return a = 0;
+        }
+
+    else
+        {
+            document.getElementById("showtext").style.display="none";
+            return a = 1;
+        }
+}
+
+var b = 1;
+
+function show_hide2() {
+    if(a==1)
+        {
+            document.getElementById("showtext2").style.display="inline";
+            return a = 0;
+        }
+    else
+        {
+            document.getElementById("showtext2").style.display="none";
+            return a = 1;
+        }
+}
+
+var c = 1;
+
+function show_hide3() {
+    if(a==1)
+        {
+            document.getElementById("showtext3").style.display="inline";
+            return a = 0;
+        }
+    else
+        {
+            document.getElementById("showtext3").style.display="none";
+            return a = 1;
+        }
+}
+
+var d = 1;
+
+function show_hide4() {
+    if(a==1)
+        {
+            document.getElementById("showtext4").style.display="inline";
+            return a = 0;
+        }
+    else
+        {
+            document.getElementById("showtext4").style.display="none";
+            return a = 1;
+        }
+}
+
+
+var e = 1;
+
+function show_hide5() {
+    if(a==1)
+        {
+            document.getElementById("showtext5").style.display="inline";
+            return a = 0;
+        }
+    else
+        {
+            document.getElementById("showtext5").style.display="none";
+            return a = 1;
+        }
+}
+
+
+var f = 1;
+
+function show_hide6() {
+    if(a==1)
+        {
+            document.getElementById("showtext6").style.display="inline";
+            return a = 0;
+        }
+    else
+        {
+            document.getElementById("showtext6").style.display="none";
+            return a = 1;
+        }
+}
